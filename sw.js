26fc4c5e8f02be11c3efbafff927fcee7fad1493
mcvo <macovogaming@gmail.com>
2018-10-29 23:33:49 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    new RegExp('https://jsonplaceholder.typicode.com/users'),
+    workbox.strategies.cacheFirst()
+);
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "css/main.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "index.html",
+    "revision": "8d81e69dc56a36f26f62c13162da5350"
+  },
+  {
+    "url": "js/app.js",
+    "revision": "fb1a059ea80ce3c769e571456e6e6764"
+  }
+]);
